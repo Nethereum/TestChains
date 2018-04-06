a3326e53f87784cf48e00f61c48020f1c92c354d
Juan Blanco <juanfranblanco@hotmail.com>
2018-04-06 12:23:27 +0100

    Initial commit

---
+++ geth-clique-windows/startgeth.bat
@@ -0,0 +1,3 @@
+RD /S /Q %~dp0\devChain\geth
+geth  --datadir=devChain init genesis_clique.json
+geth --nodiscover --rpc --datadir=devChain  --rpccorsdomain "*" --mine --rpcapi "eth,web3,personal,net,miner,admin,debug" --unlock 0x12890d2cce102216644c59daE5baed380d84830c --password "pass.txt" --verbosity 0 console  
